e76c7ebd7e60c0117e960958f2c7671e64e084f3
FiejdaszBartosz <bartosz.fiejdasz1@gmail.com>
2022-04-18 18:11:32 +0200

    All tests passed successfully

--- TestDrivenDevelopment/src/pl/retsuz/conversions/Converter.java
+++ TestDrivenDevelopment/src/pl/retsuz/conversions/Converter.java
@@ -3,6 +3,6 @@ package pl.retsuz.conversions;
 public class Converter {
 
     public String convert(GenericNumeralSystem from, GenericNumeralSystem to, String value){
-        return "I";
+        return to.fromArabic(from.toArabic(value));
     }
 }
--- TestDrivenDevelopment/src/pl/retsuz/conversions/roman/Roman.java
+++ TestDrivenDevelopment/src/pl/retsuz/conversions/roman/Roman.java
@@ -2,19 +2,85 @@ package pl.retsuz.conversions.roman;
 
 import pl.retsuz.conversions.GenericNumeralSystem;
 
+import java.util.Locale;
+
 public class Roman implements GenericNumeralSystem {
-    private int mArabicNumbers[] = {1, 5, 10, 50, 100, 500, 1000};
-    private char mRomaNumbers[] = {'I', 'V', 'X', 'L', 'C', 'D', 'M'};
+    private String mThousands[] = {"", "M", "MM", "MMM"};
+    private String mHundreds[] = {"", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"};
+    private String mDozens[] = {"", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"};
+    private String mUnity[] = {"", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"};
 
     @Override
     public String fromArabic(int val) {
-        return "I";
-        //throw new UnsupportedOperationException();
+        String result = "";
+        int temp;
+
+        if (val < 0 || val  > 3999)
+            throw new IllegalArgumentException();
+
+        result = mThousands[val / 1000]
+                + mHundreds[(val % 1000) / 100]
+                + mDozens[(val % 100) / 10]
+                + mUnity[val % 10];
+
+        return result;
     }
 
     @Override
     public int toArabic(String val) {
-        return 1;
-        //throw new UnsupportedOperationException();
+        int result = 0, previous = 0;
+        char temp;
+        val = val.toUpperCase(Locale.ROOT);
+
+        for (int i = val.length() - 1; i >= 0; i--) {
+            temp = val.charAt(i);
+            switch (temp) {
+                case 'M' -> {
+                    result = specifyNumber(1000, previous, result);
+                    previous = 1000;
+                }
+                case 'D' -> {
+                    result = specifyNumber(500, previous, result);
+                    previous = 500;
+                }
+                case 'C' -> {
+                    result = specifyNumber(100, previous, result);
+                    previous = 100;
+                }
+                case 'L' -> {
+                    result = specifyNumber(50, previous, result);
+                    previous = 50;
+                }
+                case 'X' -> {
+                    result = specifyNumber(10, previous, result);
+                    previous = 10;
+                }
+                case 'V' -> {
+                    result = specifyNumber(5, previous, result);
+                    previous = 5;
+                }
+                case 'I' -> {
+                    result = specifyNumber(1, previous, result);
+                    previous = 1;
+                }
+                default -> throw new IllegalArgumentException();
+            }
+        }
+        return result;
+    }
+
+    /**
+     * Chooses whether the current number should be subtracted or added to the result.
+     * @param number
+     * @param previous
+     * @param result
+     * @return
+     */
+    private int specifyNumber(int number, int previous, int result) {
+        if (previous > number) {
+            return result - number;
+        } else {
+            return  result + number;
+        }
     }
 }
