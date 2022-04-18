32320353d5c39c21e8261b69fe6e0ab3b0e35a33
FiejdaszBartosz <bartosz.fiejdasz1@gmail.com>
2022-04-18 12:16:58 +0200

    Finish urnfield

--- TestDrivenDevelopment/src/pl/retsuz/conversions/roman/Roman.java
+++ TestDrivenDevelopment/src/pl/retsuz/conversions/roman/Roman.java
@@ -3,6 +3,9 @@ package pl.retsuz.conversions.roman;
 import pl.retsuz.conversions.GenericNumeralSystem;
 
 public class Roman implements GenericNumeralSystem {
+    private int mArabicNumbers[] = {1, 5, 10, 50, 100, 500, 1000};
+    private char mRomaNumbers[] = {'I', 'V', 'X', 'L', 'C', 'D', 'M'};
+
     @Override
     public String fromArabic(int val) {
         return "I";
--- TestDrivenDevelopment/src/pl/retsuz/conversions/urnfield/Urnfield.java
+++ TestDrivenDevelopment/src/pl/retsuz/conversions/urnfield/Urnfield.java
@@ -5,11 +5,51 @@ import pl.retsuz.conversions.GenericNumeralSystem;
 public class Urnfield implements GenericNumeralSystem {
     @Override
     public String fromArabic(int val) {
-        return "";
+        String one = "";
+        String five = "";
+
+        if (val > 0 && val <= 29) {
+            while (val > 4) {
+                val -= 5;
+                five += "\\";
+            }
+            while (val > 0) {
+                val -= 1;
+                one += "/";
+            }
+        } else
+            throw new UnsupportedOperationException();
+
+        return one + five;
     }
 
     @Override
     public int toArabic(String val) {
-        return 0;
+        int result = 0;
+        char[] number = new char[val.length()];
+
+        // max input length
+        if (val.length() > 14)
+            throw new IllegalArgumentException();
+
+        for (int i = 0; i < val.length(); i++) {
+            int j = i + 1;
+
+            // input could be \ or /
+            // exception / after \
+            if (val.charAt(i) == '\\' || val.charAt(i) == '/' || !(val.charAt(i) == '\\' || val.charAt(j) == '/'))
+                number[i] = val.charAt(i);
+            else
+                throw new UnsupportedOperationException();
+        }
+
+        for (char i : number) {
+            if (i == '\\')
+                result += 5;
+            else
+                result += 1;
+        }
+
+        return result;
     }
 }
