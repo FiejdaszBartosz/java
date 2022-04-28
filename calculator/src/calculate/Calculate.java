89d341ff5145dfa347b790ffdd246c8ea6a5daef
FiejdaszBartosz <bartosz.fiejdasz1@gmail.com>
2022-04-28 16:27:05 +0200

    calculate function works for integer. Float to do

--- calculator/src/calculate/Calculate.java
+++ calculator/src/calculate/Calculate.java
@@ -5,29 +5,49 @@ import java.util.Stack;
 public class Calculate {
     public static double compute(char symbol, double a, double b) {
         switch (symbol) {
-            case '+' -> { return a + b; }
-            case '-' -> { return a - b; }
-            case 'x' -> { return a * b; }
-            case '/' -> { return a / b; }
-            default -> { return -1; } // error to do
+            case '+' -> {
+                return a + b;
+            }
+            case '-' -> {
+                return a - b;
+            }
+            case 'x' -> {
+                return a * b;
+            }
+            case '/' -> {
+                return a / b;
+            }
+            default -> {
+                return -1;
+            } // error to do
         }
     }
 
     public static double calculateExpression(String input) {
-        Stack<Character> stack = new Stack<>();
+        Stack<String> stack = new Stack<>();
         double result = 0;
 
         for (int i = 0; i < input.length(); i++) {
             char character = input.charAt(i);
 
             if (Character.isLetterOrDigit(character))
-                stack.push(character);
+                stack.push(String.valueOf(character));
             else {
-                char x = 'x';
-                double temp = (double) x;
-                //stack.push();
+                String firstNumber = stack.pop();
+                if (stack.peek().charAt(0) == '.') {
+
+                    // to do float operations
+
+                } else {
+                    String secondNumber = stack.pop();
+                    double newNumber = compute(character
+                            , Double.valueOf(firstNumber)
+                            , Double.valueOf(secondNumber));
+                    stack.push(String.valueOf(newNumber));
+                }
+
             }
         }
-        return 0;
+        return Double.valueOf(stack.pop());
     }
 }
