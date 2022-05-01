31d757551bea59ccf03b8b5f9407e8daf5b0f393
FiejdaszBartosz <bartosz.fiejdasz1@gmail.com>
2022-05-01 12:52:12 +0200

    Added layout

---
+++ calculator/src/view/CalculatorForm.java
@@ -0,0 +1,31 @@
+package view;
+
+import javax.swing.*;
+
+public class CalculatorForm {
+    private JPanel mainPanel;
+    private JTextField textField1;
+    private JButton undoButton;
+    private JButton clearButton;
+    private JButton openParenthesisButton;
+    private JButton closeParenthesisButton;
+    private JButton button7;
+    private JButton button8;
+    private JButton button9;
+    private JButton divisionButton;
+    private JButton button4;
+    private JButton button5;
+    private JButton button6;
+    private JButton multiplicationButton;
+    private JButton button1;
+    private JButton button2;
+    private JButton button3;
+    private JButton subtractionButton;
+    private JButton button0;
+    private JButton commaButton;
+    private JButton equalButton;
+    private JButton additionButton;
+    private JPanel ButtonsPanel;
+    private JPanel ExitPanel;
+    private JButton exitButton;
+}
